5411cff36217a7f44ce6120ea8e5dbe3d20f7a64
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-11 00:14:42 +0200

    added widgets section, working on firebase

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import Image from './images/yasmin.jpeg'
 
 import './TweetBox.css'; 
@@ -7,14 +7,18 @@ import Post from './Post'
 import { Avatar, Button } from '@material-ui/core'
 
 const TweetBox = () => {
+    const [ tweetMessage, setTweetMessage ] = useState(''); 
+    const [ tweetImage, setTweetImage ] = useState(''); 
+
+    console.log(tweetMessage, 'tweetMessage');
     return (
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
                     <Avatar src={Image} />
-                    <input placeholder="What's happening" type="text"/> 
+                    <input value={tweetMessage} onChange={(e) => setTweetMessage(e.target.value)} placeholder="What's happening" type="text"/> 
                 </div>
-                <input className="tweetBox__imageInput" placeholder="Optional: Enter Image URL" type="text"/>
+                <input value={tweetImage} onChange={e => e.target.value} className="tweetBox__imageInput" placeholder="Optional: Enter Image URL" type="text"/>
                 <Button variant="outlined" className="tweetBox__tweetButton">Tweet</Button>
             </form>
         </div>
