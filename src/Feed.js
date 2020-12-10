a719482d218f313e06dfb3a0cbdaf3f42226e9a2
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-10 16:01:21 +0200

    done with feed

--- src/Feed.js
+++ src/Feed.js
@@ -11,6 +11,15 @@ const Feed = () => {
         </div>
        <TweetBox />
        <Post />
+       <Post />
+       <Post />
+       <Post />
+       <Post />
+       <Post />
+       <Post />
+       <Post />
+       <Post />
+       <Post />
       </div>
       
 
