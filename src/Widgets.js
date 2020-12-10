1f8d8ac6be32cfe6d184e6e361315e119ff293b9
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-10 16:48:01 +0200

    done with feed

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,23 @@
 import React from 'react'; 
 
+import SearchIcon from '@material-ui/icons/Search'
+
+import {  
+    TwitterTimelineEmbed, 
+    TwitterShareButton, 
+    TwitterTweetEmbed
+} from 'react-twitter-embed';
+
 const Widgets = () => {
     return (
         <div className="widgets">
-             <h2>Widgets</h2>
-            
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />  
+                <input placeholder="Search Twitter" type="text" /> 
+            </div>
+            <div className="widgets__widgetContainer">
+                
+            </div>
         </div>
        
     )
