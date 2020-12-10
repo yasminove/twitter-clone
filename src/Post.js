c6e8ca48538db3ac4f8cf81141064d70de1d19a8
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-10 16:02:10 +0200

    done with feed

--- src/Post.js
+++ src/Post.js
@@ -1,35 +1,53 @@
-import React from 'react'; 
+import React from 'react';
+
+import './Post.css'
+import Image from './images/yasmin.jpeg'
 import { Avatar } from '@material-ui/core';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser'
-const Post = ({ 
-    displayName, 
-    username, 
-    timestamp, 
-    verified, 
-    text, 
-    image, 
-    avatar
- }) => {
-    return (
-        <div className="post">
-            <div className="post__avatar">
-               <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
-            </div>
 
-            <div className="post__header">
-                <div className="post__headerText">
-                    <h3>Yasmin Hillis <span>
-                        <VerifiedUserIcon /> @yasminhillis
-                    </span></h3>
-                    
-                </div>
-            </div>
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
 
-            <div className="post_headerDescription">
-                <p>I challenge you to build a twitte clone</p>
-            </div>
+const Post = ({
+  displayName,
+  username,
+  timestamp,
+  verified,
+  text,
+  image,
+  avatar,
+}) => {
+  return (
+    <div className='post'>
+      <div className='post__avatar'>
+        <Avatar src={Image} />
+      </div>
+      <div className='post__body'>
+        <div className='post__header'>
+          <div className='post__headerText'>
+            <h3>
+              Yasmin Hillis
+              <span className="post__headerSpecial">
+                <VerifiedUserIcon className="post__badge"/> @yasminhillis
+              </span>
+            </h3>
+          </div>
+          <div className='post_headerDescription'>
+            <p>I challenge you to build a twitter clone</p>
+          </div>
         </div>
-    )
-}
+
+        <div className='post__footer'>
+          <ChatBubbleOutlineIcon fontSize='small' />
+          <RepeatIcon fontSize='small' />
+          <FavoriteBorderIcon fontSize='small' />
+          <PublishIcon fontSize='small' />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Post;
